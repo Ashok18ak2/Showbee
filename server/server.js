290de63452f652194ddfef66e5687acdb00996cf
Ashok18ak2 <ashokak2702@gmail.com>
2026-02-26 15:36:22 +0530

    fixed controller

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import showRouter from './routes/showRoutes.js';
-import bookingRouter from './routes/bookingRoutes,js';
+import bookingRouter from './routes/bookingRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
@@ -21,7 +21,7 @@ app.use(clerkMiddleware())
 
 
 
-app.get('/',(req,res)=>res.send('server2 is Live!'))
+app.get('/',(req,res)=>res.send('server is Live!'))
 app.use('/api/inngest',serve({ client: inngest, functions }));
 app.use('/api/show',showRouter)
 app.use('/api/booking',bookingRouter);

f6188995f50c1ac96ad805860e2fd842bad8a2df
Ashok18ak2 <ashokak2702@gmail.com>
2026-03-01 11:37:09 +0530

    booking confirmation mail added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,7 @@
-import Booking from '../models/Booking.js';
-import Show from '../models/Show.js';
-import  User  from '../models/User.js'
+import sendEmail from "../configs/nodeMailer.js";
+import Booking from "../models/Booking.js";
+import Show from "../models/Show.js";
+import User from "../models/User.js";
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
@@ -8,74 +9,127 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
 //Inngest functio to save user data
 const syncUserCreation = inngest.createFunction(
-    {id:'sync-user-from-clerk'},
-    {event:'webhook-integration/user.created'},
-    async({event})=>{
-        const {id,first_name,last_name,email_addresses,image_url } = event.data
-        const userData = {
-            _id:id,
-            email:email_addresses[0].email_address,
-            name:first_name + ' ' + last_name,
-            image:image_url
-        }
-        await User.create(userData);
-    }
-)
+  { id: "sync-user-from-clerk" },
+  { event: "webhook-integration/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.create(userData);
+  },
+);
 
 //Inngest function to delete user data
 
 const syncUserDeletion = inngest.createFunction(
-    {id:'delete-user-from-clerk'},
-    {event:'webhook-integration/user.deleted'},
-    async({event})=>{
-        const {id}= event.data
-        await User.findByIdAndDelete(id)
-        
-    }
-)
+  { id: "delete-user-from-clerk" },
+  { event: "webhook-integration/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
+  },
+);
 
 //Inngest function to update user data
 
-
 const syncUserUpdation = inngest.createFunction(
-    {id:'update-user-from-clerk'},
-    {event:'webhook-integration/user.updated'},
-    async({event})=>{
-        const {id,first_name,last_name,email_addresses,image_url } = event.data
-        const userData = {
-            _id:id,
-            email:email_addresses[0].email_address,
-            name:first_name + ' ' + last_name,
-            image:image_url
-        }
-        await User.findByIdAndUpdate(id,userData)
-
-    }
-)
+  { id: "update-user-from-clerk" },
+  { event: "webhook-integration/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.findByIdAndUpdate(id, userData);
+  },
+);
 
 //Inngest Function to cancel booking and release seats of show sfter 10 moniutes pf booking created if payment is not made
 
 const releaseSeatsAndDeleteBooking = inngest.createFunction(
-    {id:'release-seats-and-delete-booking'},
-    {event:"app/checkpayment"},
-    async({event,step})=>{
-        const tenMinutesLater = new Date(Date.now() + 10 *60 *1000);
-        await step.sleepUntil('wait-for-10-minutes',tenMinutesLater);
+  { id: "release-seats-and-delete-booking" },
+  { event: "app/checkpayment" },
+  async ({ event, step }) => {
+    const tenMinutesLater = new Date(Date.now() + 10 * 60 * 1000);
+    await step.sleepUntil("wait-for-10-minutes", tenMinutesLater);
 
-        await step.run('check-payment-status',async ()=>{
-            const bookingId = event.data.bookingId;
-            const booking = await Booking.findById(bookingId)
+    await step.run("check-payment-status", async () => {
+      const bookingId = event.data.bookingId;
+      const booking = await Booking.findById(bookingId);
 
-            if(!booking.isPaid){
-            const show = await Show.findById(booking.show);
-            booking.bookedSeats.forEach((seat)=>{
-                delete show.occupiedSeats[seat]
-            });
-            show.markModified('occupiedSeats')
-            await show.save()
-            await Booking.findByIdAndDelete(booking._id)
-            }
-        })
-    }
-)
-export const functions = [syncUserCreation,syncUserDeletion,syncUserUpdation,releaseSeatsAndDeleteBooking];
+      if (!booking.isPaid) {
+        const show = await Show.findById(booking.show);
+        booking.bookedSeats.forEach((seat) => {
+          delete show.occupiedSeats[seat];
+        });
+        show.markModified("occupiedSeats");
+        await show.save();
+        await Booking.findByIdAndDelete(booking._id);
+      }
+    });
+  },
+);
+
+//Inngest function to sent email when the user book a show
+
+const sendBookingConfirmationEmail = inngest.createFunction(
+  { id: "send-booking-confirmation-email" },
+  { event: "app/show.booked" },
+  async ({ event, step }) => {
+    const { bookingId } = event.data;
+
+    const booking = await Booking.findById(bookingId)
+      .populate({
+        path: "show",
+        populate: { path: "movie", model: "Movie" },
+      })
+      .populate("user");
+
+    await sendEmail({
+      to: booking.user.email,
+      subject: `Payment Confirmation : ${booking.show.movie.title} booked!`,
+      body: `
+  <div style="font-family: Arial, sans-serif; line-height: 1.5;">
+    <h2>Hi ${booking.user.name},</h2>
+
+    <p>
+      Your booking for <strong style="color: #F84565;">
+        "${booking.show.movie.title}"
+      </strong> is confirmed.
+    </p>
+
+    <p>
+      <strong>Date:</strong>
+      ${new Date(booking.show.showDateTime).toLocaleDateString("en-US", {
+        timeZone: "Asia/Kolkata",
+      })}<br/>
+
+      <strong>Time:</strong>
+      ${new Date(booking.show.showDateTime).toLocaleTimeString("en-US", {
+        timeZone: "Asia/Kolkata",
+      })}
+    </p>
+
+    <p>Enjoy the show 🍿</p>
+    <p>Thanks for booking with us!<br/>— QuickShow Team</p>
+  </div>
+`,
+    });
+  },
+);
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+  releaseSeatsAndDeleteBooking,
+  sendBookingConfirmationEmail
+];
